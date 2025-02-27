875b519a72791d489f3bf5fe9c59e90498886ff6
naoya.oda <naoya.oda@e-seikatsu.co.jp>
2025-02-27 09:45:47 +0900

    Chrome拡張のバージョンアップに対応

--- js/background.js
+++ js/background.js
@@ -1,3 +1,3 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.sendMessage(tab.id, "true");
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.sendMessage(tab.id, "true");
 });
--- js/content.js
+++ js/content.js
@@ -1,30 +1,28 @@
-chrome.extension.onMessage.addListener(function(request) {
-	if (request == "true") {
-		change_to_oda();
-	}
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request === "true") {
+    change_to_oda();
+  }
 });
 
 const urlList = [
-	"../images/P1060105-removebg.png",
-	"../images/P1060115-removebg.png",
-	"../images/P1060120-removebg.png",
-	"../images/P1060135-removebg.png",
-	"../images/P1060152-removebg.png",
-	"../images/P1060175-removebg.png",
-	"../images/P1060178-removebg.png",
-	"../images/P1060198-removebg.png",
-	"../images/P1060203-removebg.png",
-	"../images/P1060215-removebg.png"
-]
+  "../images/P1060105-removebg.png",
+  "../images/P1060115-removebg.png",
+  "../images/P1060120-removebg.png",
+  "../images/P1060135-removebg.png",
+  "../images/P1060152-removebg.png",
+  "../images/P1060175-removebg.png",
+  "../images/P1060178-removebg.png",
+  "../images/P1060198-removebg.png",
+  "../images/P1060203-removebg.png",
+  "../images/P1060215-removebg.png",
+];
 
 function change_to_oda() {
-	imgs = document.querySelectorAll("img")
-	let i = 0;
-	let url = "";
-	imgs.forEach(function(element) {
-		i = Math.floor(Math.random()*urlList.length);
-		url = chrome.extension.getURL(urlList[i]);
-		element.setAttribute("src", url)
-		element.removeAttribute("srcset")
-		});
+  const imgs = document.querySelectorAll("img");
+  imgs.forEach((element) => {
+    const i = Math.floor(Math.random() * urlList.length);
+    const url = chrome.runtime.getURL(urlList[i]);
+    element.setAttribute("src", url);
+    element.removeAttribute("srcset");
+  });
 }
